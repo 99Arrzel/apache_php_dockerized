36c5a9de0b9fa2f60d626cf8b99dae99938064f8
Andres Carrillo <af.carrillo@live.com>
2024-08-10 10:07:01 -0400

    Add new version

--- build.sh
+++ build.sh
@@ -1,3 +1,5 @@
 # Build the current dockerfiles.
-docker build -t 'php8.3-dev' -f dockerfile-development .
-docker build -t 'php8.3-prod' -f dockerfile-production .
+docker build -t 'php8.3-dev' -f dockerfile-development-83 .
+docker build -t 'php8.3-prod' -f dockerfile-production-83 .
+docker build -t 'php7.4-dev' -f dockerfile-development-74 .
+docker build -t 'php7.4-prod' -f dockerfile-production-74 .
--- index.php
+++ index.php
@@ -1,3 +1,6 @@
 <?php
+$hiddenvar = 123;
 echo "Hello, World!";
 echo xdebug_info();
+
+phpinfo();
